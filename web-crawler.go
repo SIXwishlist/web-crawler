49288bcac8851544cba179fed72c9e9e7471f5eb
jafrog <howeveririna@gmail.com>
2016-08-05 17:01:31 +0100

    As in the book - deadlock

--- web-crawler.go
+++ web-crawler.go
@@ -1,34 +1,25 @@
 package main
 
 import (
-	"fmt"
 	"os"
 )
 
 func main() {
-	if len(os.Args) <= 1 {
-		fmt.Println("Please provide a website url")
-		return
-	}
+	worklist := make(chan []string)
 
-	rootUrl := os.Args[1]
+	// Start with the command-line arguments.
+	go func() { worklist <- os.Args[1:] }()
 
-	visitedLinks := make(map[string]bool)
-	links := make(chan string, 100)
-
-	go func() { links <- rootUrl }()
-
-	for link := range links {
-		fmt.Println("Link to visit: ", link)
-		if !visitedLinks[link] {
-			visitedLinks[link] = true
-			go func(link string) {
-				newLinks := crawl(link)
-				for i := 0; i < len(newLinks); i++ {
-					newLink := newLinks[i]
-					fmt.Println("newLink: ", newLink)
-				}
-			}(link)
+	// Crawl the web concurrently.
+	seen := make(map[string]bool)
+	for list := range worklist {
+		for _, link := range list {
+			if !seen[link] {
+				seen[link] = true
+				go func(link string) {
+					worklist <- crawl(link)
+				}(link)
+			}
 		}
 	}
 }
