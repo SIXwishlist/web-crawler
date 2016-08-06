f72bb2393989f98327bcb64a8ec382cd74b6514c
jafrog <howeveririna@gmail.com>
2016-08-06 14:08:02 +0100

    Trying to terminate when channels are empty

--- web-crawler.go
+++ web-crawler.go
@@ -6,20 +6,39 @@ import (
 
 func main() {
 	worklist := make(chan []string)
+	unseenLinks := make(chan string)
 
 	// Start with the command-line arguments.
 	go func() { worklist <- os.Args[1:] }()
 
+	for i:= 0; i < 20; i++ {
+		go func() {
+			for {
+				link, ok := <-unseenLinks
+				if ok {
+					foundLinks := crawl(link)
+					worklist <- foundLinks
+				} else {
+					return
+				}
+			}
+		}()
+	}
+
 	// Crawl the web concurrently.
 	seen := make(map[string]bool)
-	for list := range worklist {
-		for _, link := range list {
-			if !seen[link] {
-				seen[link] = true
-				go func(link string) {
-					worklist <- crawl(link)
-				}(link)
+	for {
+		list, ok := <-worklist
+		if ok {
+			for _, link := range list {
+				if !seen[link] {
+					seen[link] = true
+					unseenLinks <- link
+				}
 			}
+		} else {
+			close(worklist)
+			close(unseenLinks)
 		}
 	}
 }
