7ecefe6f4e67e75badd3ef142d770b8634179bdc
jafrog <howeveririna@gmail.com>
2016-08-07 13:14:19 +0100

    Version with deadlocks happening when workers < links
    When no. of workers < no. of found links main thread tries to write into
    `unseenLinks` channel while all of the workers are still busy. That
    leads to a deadlock because worker is trying to write to the `worklist`
    channel (also unbuffered).

--- crawl.go
+++ crawl.go
@@ -4,10 +4,12 @@ import(
 	"fmt"
 )
 
-func crawl(link string, worklist chan<- []string) {
-	fmt.Println("Crawling ", link)
-	links := make([]string, 2)
-	links[0] = "foo"
-	links[1] = "bar"
+func crawl(id int, link string, worklist chan<- []string) {
+	fmt.Println("Worker #", id, "crawling", link)
+	links := make([]string, 3)
+	links[0] = "Link1"
+	links[1] = "Link2"
+	links[2] = "Link3"
+	fmt.Println("Worker #", id, "found", links)
 	worklist <- links
 }
--- web-crawler.go
+++ web-crawler.go
@@ -1,20 +1,25 @@
 package main
 
 import (
-	"os"
+	"flag"
+	"fmt"
 )
 
 func main() {
 	worklist := make(chan []string)
-	unseenLinks := make(chan string)
+	unseenLinks := make(chan string, 1)
 
-	// Start with the command-line arguments.
-	go func() { worklist <- os.Args[1:] }()
+	workers := flag.Int("w", 1, "Number of concurrent workers to perform requests")
+	url := flag.String("u", "", "Starting URL")
+	flag.Parse()
 
-	for i:= 0; i < 20; i++ {
+	go func() { worklist <- []string{*url} }()
+
+	fmt.Println("No of workers:", *workers)
+	for i:= 0; i < *workers; i++ {
 		go func() {
 			for link := range unseenLinks {
-				crawl(link, worklist)
+				crawl(i, link, worklist)
 			}
 		}()
 	}
@@ -23,11 +28,16 @@ func main() {
 	seen := make(map[string]bool)
 
 	for n := 1; n > 0; n-- {
+		fmt.Println("n", n)
 		list := <-worklist
+		fmt.Println("Got list", list)
 		for _, link := range list {
+			fmt.Println("Checking link", link)
+			fmt.Println("Seen is", seen)
 			if !seen[link] {
 				seen[link] = true
 				n++
+				fmt.Println("Adding", link)
 				unseenLinks <- link
 			}
 		}
