3fd1fbd2aae3a35c16e026126f3095e4ac1792f1
jafrog <howeveririna@gmail.com>
2016-08-07 18:54:22 +0100

    Adds integration test for a web crawler
    - Creates `startWorkers` and `dispatchLinks` for easier testing
    - Tests the case when the number of workers is less than number of links
    - to traverse
    - Assumes that tests wait for all goroutines to finish. (I tried adding
    - sleep to the `testWorker` and it still worked)

--- web-crawler.go
+++ web-crawler.go
@@ -4,6 +4,28 @@ import (
 	"flag"
 )
 
+type workerFn func(int, <-chan string, chan<- []string)
+
+func startWorkers(workers int, worker workerFn, unseenLinks <-chan string, foundLinks chan<- []string) {
+	for i:= 0; i < workers; i++ {
+		go worker(i, unseenLinks, foundLinks)
+	}
+}
+
+func dispatchLinks(startingUrl string, foundLinks chan []string, unseenLinks chan<- string, seen map[string]bool) {
+	go func() { foundLinks <- []string{startingUrl} }()
+	for n := 1; n > 0; n-- {
+		list := <-foundLinks
+		for _, link := range list {
+			if !seen[link] {
+				seen[link] = true
+				n++
+				unseenLinks <- link
+			}
+		}
+	}
+}
+
 var (
 	workers = flag.Int("w", 1, "Number of concurrent workers to perform requests")
 	startingUrl = flag.String("u", "", "Starting URL")
@@ -18,20 +40,6 @@ func main() {
 
 	flag.Parse()
 
-	go func() { foundLinks <- []string{*startingUrl} }()
-
-	for i:= 0; i < *workers; i++ {
-		go worker(i, unseenLinks, foundLinks)
-	}
-
-	for n := 1; n > 0; n-- {
-		list := <-foundLinks
-		for _, link := range list {
-			if !seen[link] {
-				seen[link] = true
-				n++
-				unseenLinks <- link
-			}
-		}
-	}
+	startWorkers(*workers, worker, unseenLinks, foundLinks)
+	dispatchLinks(*startingUrl, foundLinks, unseenLinks, seen)
 }
--- web-crawler_test.go
+++ web-crawler_test.go
@@ -2,12 +2,29 @@ package main
 
 import "testing"
 
+func testWorker(id int, unseenLinks <-chan string, foundLinks chan<- []string) {
+	for _ = range unseenLinks {
+		links := make([]string, 3)
+		links[0] = "Link1"
+		links[1] = "Link2"
+		links[2] = "Link3"
+		go func() { foundLinks <- links }()
+	}
+}
+
 func TestWebCrawler(t *testing.T) {
-	*workers = 2
-	*startingUrl = "http://tomblomfield.com/"
-	// test that crawler spawns 2 workers
-	// each worker returns 3 links
-	// all links are crawled
-	// and program terminates with 0
-	main()
+	var (
+		startingUrl = "http://tomblomfield.com/"
+		workers = 2
+		foundLinks = make(chan []string)
+		unseenLinks = make(chan string)
+		seen = make(map[string]bool)
+	)
+
+	startWorkers(workers, testWorker, unseenLinks, foundLinks)
+	dispatchLinks(startingUrl, foundLinks, unseenLinks, seen)
+
+	if len(seen) < 4 {
+		t.Error("Not all links have been traversed")
+	}
 }
