4bb1fcce8e42dd52a517933ede5eb99b817d0f8b
jafrog <howeveririna@gmail.com>
2016-08-10 15:41:43 +0100

    Extracts link selection to a function

--- html-doc.go
+++ html-doc.go
@@ -19,6 +19,23 @@ func (this htmlDoc) ReadBody() string {
 	return "<html></html>"
 }
 
+func selectLinks(n *html.Node, buf []string) []string {
+	fmt.Println(n.Data)
+	if n.Type == html.ElementNode && n.Data == "a" {
+		for _, attr := range n.Attr {
+			if attr.Key == "href" {
+				fmt.Println(attr.Val)
+				buf = append(buf, attr.Val)
+			}
+		}
+	}
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		buf = selectLinks(c, buf)
+	}
+	return buf
+}
+
 func (this htmlDoc) ExtractLinks() []string {
 	var links []string
 
@@ -27,21 +44,7 @@ func (this htmlDoc) ExtractLinks() []string {
 		return nil
 	}
 
-	var f func(*html.Node)
-	f = func(n *html.Node) {
-		if n.Type == html.ElementNode && n.Data == "a" {
-			for _, attr := range n.Attr {
-				if attr.Key == "href" {
-					fmt.Println(attr.Val)
-					links = append(links, attr.Val)
-				}
-			}
-		}
-		for c := n.FirstChild; c != nil; c = c.NextSibling {
-			f(c)
-		}
-	}
-	f(doc)
+	links = selectLinks(doc, links)
 
 	return links
 }
