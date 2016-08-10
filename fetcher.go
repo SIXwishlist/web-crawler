3ed168ee3cde1eb95f0bd6a6f3fc23d74fa94a04
jafrog <howeveririna@gmail.com>
2016-08-10 11:21:08 +0100

    Minor bug fixes + make fetcher work on real pages

--- fetcher.go
+++ fetcher.go
@@ -24,17 +24,20 @@ func (this fetcher) Fetch(url string) (string, error) {
 		client = &http.Client{}
 	}
 
-	response, err := this.client.Get(url)
+	response, err := client.Get(url)
 	defer response.Body.Close()
 
 	if err != nil {
 		return "", fmt.Errorf("Error fetching %s: %s", url, err)
 	}
 
-	body, err = ioutil.ReadAll(response.Body)
+	body, err := ioutil.ReadAll(response.Body)
 	if err != nil {
 		return "", fmt.Errorf("Error reading %s: %s", url, err)
 	}
 
-	return string(body[0:response.ContentLength]), nil
+	if response.ContentLength > 0 {
+		body = body[0:response.ContentLength]
+	}
+	return string(body), nil
 }
